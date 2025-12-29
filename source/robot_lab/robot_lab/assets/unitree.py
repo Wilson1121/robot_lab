7e8b6227e1ee8e5f90888693831319ab9e8b87f8
Wilson1121 <1723934625@qq.com>
2025-12-30 00:54:40 +0800

    添加了Go2Arm的asset资产配置

--- source/robot_lab/robot_lab/assets/unitree.py
+++ source/robot_lab/robot_lab/assets/unitree.py
@@ -6,7 +6,7 @@ Reference: https://github.com/unitreerobotics/unitree_ros
 """
 
 import isaaclab.sim as sim_utils
-from isaaclab.actuators import DCMotorCfg, ImplicitActuatorCfg
+from isaaclab.actuators import DCMotorCfg, ImplicitActuatorCfg, DelayedPDActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
 
 from robot_lab.assets import ISAACLAB_ASSETS_DATA_DIR
@@ -15,6 +15,71 @@ from robot_lab.assets import ISAACLAB_ASSETS_DATA_DIR
 # Configuration
 ##
 
+# 本质上算asset_cfg.py的功能
+
+UNITREE_Go2Arm_CFG = ArticulationCfg(
+    spawn=sim_utils.UrdfFileCfg(
+        fix_base=False,
+        merge_fixed_joints=True,
+        replace_cylinders_with_capsules=False,
+        asset_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/unitree/Go2Arm_description/urdf/go2_piper_description.urdf",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, 
+            solver_position_iteration_count=8, 
+            solver_velocity_iteration_count=4,
+        ),
+        joint_drive=sim_utils.UrdfConverterCfg.JointDriveCfg(
+            gains=sim_utils.UrdfConverterCfg.JointDriveCfg.PDGainsCfg(stiffness=0, damping=0)
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.4),
+        joint_pos={
+            ".*_hip_joint": 0.0,
+            ".*_thigh_joint": 1.0,
+            ".*_calf_joint": -1.8,
+            # Piper arm joints (joint1..joint6)
+            "joint[1-6]": 0.0,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    # 按 URDF 名称分两组执行器：
+    # - legs: 12 个腿部关节（*_hip_joint, *_thigh_joint, *_calf_joint）
+    # - upper: 6 个机械臂关节（joint1..joint6）
+    actuators={
+        "legs": DelayedPDActuatorCfg(
+            joint_names_expr=[r".*_hip_joint", r".*_thigh_joint", r".*_calf_joint"],
+            effort_limit=50,
+            velocity_limit=28,
+            stiffness=30.0,
+            damping=1.5,
+            friction=0.2,
+            min_delay=0,
+            max_delay=0,
+        ),
+        "upper": DelayedPDActuatorCfg(
+            joint_names_expr=[r"joint[1-6]"],
+            effort_limit=50,
+            velocity_limit=28,
+            stiffness=0.0,
+            damping=30,
+            friction=0.2,
+            min_delay=0,
+            max_delay=0,
+        ),
+    },
+)
 
 UNITREE_A1_CFG = ArticulationCfg(
     spawn=sim_utils.UrdfFileCfg(
